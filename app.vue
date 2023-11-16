a9efdb1fceb75cb8a629a0ad84bef6b5963e5015
Wilson <wtovaragudelo@gmail.com>
2023-11-16 17:43:33 -0500

    feat: index

--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
 <template>
-  <AppTopbar />
+  <div class="w-full flex flex-col min-h-screen">
+    <AppTopbar />
 
-  <div class="w-full p-5">
-    <div class="w-full max-w-xl m-auto">
-      <NuxtPage />
+    <div class="w-full p-5 flex align-middle justify-center flex-1">
+      <div class="w-full max-w-xl pt-20">
+        <NuxtPage />
+      </div>
     </div>
-  </div>
 
-  <AppFooter />
+    <AppFooter />
+  </div>
 </template>
 
 <script setup lang="ts">
